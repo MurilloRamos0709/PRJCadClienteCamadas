6fe1e04900fb2e637e2ebd31f5bac5d9b9eb1cd9
MurilloRamos <Mr.murillorar.007@gmail.com>
2019-05-14 17:23:50 -0300

    criação do corpo do metodo atualizar do CRUD

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -1,5 +1,112 @@
-package com.prjcadcliente.persistencia;
+          package com.prjcadcliente.persistencia;
 
-public class CRUDCliente {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import com.prjcadcliente.dominio.Cliente;
+
+/**
+ * <b>CRUDCliente</b><br>
+ * Essa classe permite manipular as informações do cliente. Aqui você encontrará os seguintes comandos:
+ * <ul>
+ * <li> Cadastro</li>
+ * <li> Pesquisar por nome e por id</li>
+ * <li> Atualizar</li>
+ * <li> Deletar</li>
+ * @author Murillo.raramos
+ *
+ */
+          
+          public class CRUDCliente {
+        	  //Declaração das instâncias de comunicação com o banco de dados
+        	  private Connection con = null;
+        	  private ResultSet res = null;
+        	  private PreparedStatement pst = null;
+        	  public String cadastrar(Cliente cliente ) {
+        		  
+        		  String msg = "";
+        		  
+        		  //Criação dos objetos para a conexao com o banco de dados
+        		  try {
+        			  Class.forName("com.mysql.cj.jdbc.Drive").newInstance();
+        			  con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+        			  
+        			  String Consulta = "INSERT INTO tbcliente(nome, email, telefone, idade)values(?,?,?,?)";
+        			  
+        			  pst = con.prepareStatement(Consulta);
+        			  
+        			  pst.setString(1,  cliente.getNome());
+        			  pst.setString(2, cliente.getEmail());
+        			  pst.setString(3, cliente.getTelefone());
+        			  int r = pst.executeUpdate();
+        		
+        			  if(r > 0)
+        				  msg = "Cadastro realizado com sucesso!";
+        			  else
+        				  msg = "Não foi possível cadastrar!";
+        			  }  
+        		  catch(SQLException ex) {
+        			  msg = "Erro ao tenar cadastrar:"+ex.getMessage();
+        		  }
+        		  catch(Exception e) {
+        			  msg = "Erro inesperado:"+e.getMessage();
+        			  }
+        		  	finally {
+        		  		try {con.close();}catch(Exception e) {e.printStackTrace();}
+        		  	}
+				return msg;
+        	  }
+        	  
+        	  
+        	  public String autalizar(Cliente cliente) {
+ String msg = "";
+        		  
+        		  //Criação dos objetos para a conexao com o banco de dados
+        		  try {
+        			  Class.forName("com.mysql.cj.jdbc.Drive").newInstance();
+        			  con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+        			  
+        			  String Consulta = "UPDATE tbcliente(nome=?, email=?, telefone=?, idade=? WHERE id=?)values(?,?,?,?)";
+        			  
+        			  pst = con.prepareStatement(Consulta);
+        			  
+        			  pst.setString(1,  cliente.getNome());
+        			  pst.setString(2, cliente.getEmail());
+        			  pst.setString(3, cliente.getTelefone());
+        			  pst.setInt(5, Cliente.getId());
+        			  int r = pst.executeUpdate();
+        		
+        			  if(r > 0)
+        				  msg = "Cadastro realizado com sucesso!";
+        			  else
+        				  msg = "Não foi possível cadastrar!";
+        			  }  
+        		  catch(SQLException ex) {
+        			  msg = "Erro ao tenar cadastrar:"+ex.getMessage();
+        		  }
+        		  catch(Exception e) {
+        			  msg = "Erro inesperado:"+e.getMessage();
+        			  }
+        		  	finally {
+        		  		try {con.close();}catch(Exception e) {e.printStackTrace();}
+        		  	}
+				return msg;
+        	  }
+        	  public String deletar(Cliente cliente) {
+        		  return null;
+        	  }
+        	  public List<Cliente> PesquisarPorNome(String nome) {
+        		  return null;
+        	  }
+        	  public List<Cliente> PesquisarPorId(int id){
+        		  return null;
+        	  }
+
+        	  public List<Cliente> PesquisarTodos(){
+        		  return null;
+        	  }
 }
